d8d195e220785f0ba0e21921ffd295d21e3eda38
ikhwanmuhammad <mochciwant123@gmail.com>
2020-06-28 13:18:48 +0700

    av

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://202.150.141.238:1324/';
+$config['base_url'] = 'http://202.150.141.238:1324/yournall';
 
 /*
 |--------------------------------------------------------------------------
