c9f143b3f7e75b3b62c9b2ec1c711ff5b0959af4
ikhwanmuhammad <mochciwant123@gmail.com>
2020-06-28 11:40:33 +0700

    config

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '202.150.141.238:1324',
 	'username' => 'root',
-	'password' => '',
+	'password' => 'KKI2018@)!*',
 	'database' => 'db_yournall',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
